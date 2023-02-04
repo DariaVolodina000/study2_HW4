c0dc98e671c1a4ba9ec7932567fac497a76f2c22
Daria <dashavolodina@gmail.com>
2023-02-04 18:02:03 +0800

    сумма цифр в числе. функция

---
+++ HW4_02/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Задача 2: Напишите программу, которая принимает 
+// на вход число и выдаёт сумму цифр в числе. 
+// Реализовать через функции.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите чило >");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int advance = 0;
+while (num > 0)
+{
+    advance = num % 10;
+    sum = sum + advance;
+    num = num / 10;
+}
+Console.WriteLine($"Сумма чисел = {sum} ");
